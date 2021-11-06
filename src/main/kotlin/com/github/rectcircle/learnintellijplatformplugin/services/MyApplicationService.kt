838e4f9178ea2bf7de590a0e37727137b4bbd93f
GitHub Action <action@github.com>
2021-11-06 03:18:22 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/rectcircle/learnintellijplatformplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.rectcircle.learnintellijplatformplugin.services
+
+import com.github.rectcircle.learnintellijplatformplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/rectcircle/learnintellijplatformplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/rectcircle/learnintellijplatformplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.rectcircle.learnintellijplatformplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rectcircle.learnintellijplatformplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
